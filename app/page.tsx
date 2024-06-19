1d43f30d018b6d813ade8c0e5c7ae430c02df55f
ixc7 <isaacxpreston7@gmail.com>
2024-06-19 10:47:40 -0400

    extract navbar to separate component

--- app/page.tsx
+++ app/page.tsx
@@ -5,45 +5,49 @@ const Button = () => (
   </div>
 )
 
+const NavBar = () => (
+  <div
+    className="
+    col-span-full bg-red-700 px-8 py-4 
+    grid grid-cols-subgrid gap-8
+    shadow-xl rounded-full
+  ">
+    <div className="col-span-3 bg-red-100">
+      <div className="h-12 bg-red-300">left logo</div>
+    </div>
+    <div className="col-span-6 flex justify-around gap-2 bg-red-200 px-5">
+      <Button />
+      <Button />
+      <Button />
+      <Button />
+      <Button />
+    </div>
+
+    <div className="col-span-3 bg-red-100 flex items-center justify-end">
+      <div className="h-12 w-12 bg-red-300 rounded-full mr-2"></div>
+      <div className="mr-3 pr-3 bg-red-400 border-r border-color-1">
+        <p>Dr. Jose Simmons</p>
+        <p>General Practitioner</p>
+      </div>
+      <div className="h-5 bg-red-500 mr-2">X</div>
+      <div className="bg-red-600">.</div>
+    </div>
+  </div>
+)
+
 const Home = () => {
   return (
-    <div className="grid grid-cols-12  bg-red-200 m-[1.125rem]">
-      <div
-        className="
-        col-span-full bg-red-700 px-8 py-3 
-        grid grid-cols-subgrid gap-8
-      ">
-        <div className="col-span-3 bg-red-100">left logo</div>
-
-        <div className="col-span-6 flex justify-around gap-10 bg-red-200">
-          <Button />
-          <Button />
-          <Button />
-          <Button />
-          <Button />
-        </div>
-
-        <div className="col-span-3 bg-red-100 text-right">right content</div>
-      </div>
-
-      <div
-        className="
-        col-span-3 bg-red-400
-        p-5
-      ">
+    <div className="grid grid-cols-12 gap-8 bg-red-200 m-[1.125rem]">
+      <NavBar />
+      <div className="col-span-3 bg-red-400 p-5">
         <h1 className="text-2xl font-extrabold">Patients</h1>
         <div className="bg-red-600 mt-10">profiles</div>
       </div>
-      <div
-        className="
-        col-span-6 bg-red-500
-      ">
+      <div className="col-span-6 bg-red-500 p-5">
         <h1 className="text-2xl font-extrabold">Diagnosis History</h1>
+        <div className="bg-red-600 mt-10">Blood Pressure</div>
       </div>
-      <div
-        className="
-        col-span-3 bg-red-300
-      ">
+      <div className="col-span-3 bg-red-300 p-5">
         <h1 className="text-2xl font-extrabold">Jessica Taylor</h1>
       </div>
     </div>
