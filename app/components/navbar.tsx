01df238e079a169040630d33556d447ecadb5bd7
ixc7 <isaacxpreston7@gmail.com>
2024-06-19 15:44:26 -0400

    render user info/pictures

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -1,7 +1,7 @@
 const NavButton = ({ className, children }) => (
   <div className="py-3 px-4 rounded-full flex gap-2 items-center body-emphasized bg-active-1">
     <div className={`aspect-square w-5 ${className}`} />
-    <div>{children}</div>
+    <p className="font-bold">{children}</p>
   </div>
 )
 
@@ -24,9 +24,16 @@ export const NavBar = () => (
     </div>
 
     <div className="col-span-3 flex items-center justify-end">
-      <div className="h-12 w-12 bg-active-1 rounded-full mr-2"></div>
+      <div
+        className="h-12 w-12 rounded-full mr-2"
+        style={{
+          backgroundSize: "cover",
+          backgroundImage:
+            "url('./senior-woman-doctor-and-portrait-smile-for-health-2023-11-27-05-18-16-utc.png')",
+        }}
+      />
       <div className="mr-3 pr-3 border-r border-color-1">
-        <p>Dr. Jose Simmons</p>
+        <p className="font-bold">Dr. Jose Simmons</p>
         <p>General Practitioner</p>
       </div>
       <div className="aspect-square w-5 mr-3 settings-icon" />
--- app/components/patients.tsx
+++ app/components/patients.tsx
@@ -1,13 +1,65 @@
-const PatientCard = () => (
-  <div className="relative flex items-center py-4 px-5 patient-card">
-    <div className="bg-active-1 w-12 h-12 rounded-full mr-3" />
-    <div>
-      <p>Firstname Lastname</p>
-      <p>Gender, Age</p>
-    </div>
-    <div className="absolute right-0 w-[1.125rem] h-1 more-horizontal-icon" />
-  </div>
-)
+const profiles = [
+  {
+    name: "Emily Williams",
+    info: "Female, 18",
+    img: "Layer 8",
+  },
+  {
+    name: "Ryan Johnson",
+    info: "Male, 45",
+    img: "Layer 1",
+  },
+  {
+    name: "Brandon Mitchell",
+    info: "Male, 36",
+    img: "Layer 3",
+  },
+  {
+    name: "Jessica Taylor",
+    info: "Female, 28",
+    img: "Layer 2",
+  },
+  {
+    name: "Samantha Johnson",
+    info: "Female, 56",
+    img: "Layer 6",
+  },
+  {
+    name: "Ashley Martinez",
+    info: "Female, 54",
+    img: "Layer 12",
+  },
+  {
+    name: "Olivia Brown",
+    info: "Female, 32",
+    img: "Layer 10",
+  },
+  {
+    name: "Tyler Davis",
+    info: "Male, 30",
+    img: "Layer 9",
+  },
+  {
+    name: "Kevin Anderson",
+    info: "Male, 30",
+    img: "Layer 4",
+  },
+  {
+    name: "Dylan Thompson",
+    info: "Male, 36",
+    img: "Layer 5",
+  },
+  {
+    name: "Nathan Evans",
+    info: "Male, 58",
+    img: "Layer 7",
+  },
+  {
+    name: "Mike Nolan",
+    info: "Male, 31",
+    img: "pexels-photo-1222271",
+  },
+]
 
 export const Patients = () => (
   <>
@@ -17,8 +69,21 @@ export const Patients = () => (
     </div>
 
     <div className="mt-10 max-h-[1054px] overflow-y-scroll">
-      {Array.from(Array(25)).map((i) => (
-        <PatientCard />
+      {profiles.map(({ name, info, img }) => (
+        <div className="relative flex items-center py-4 px-5 patient-card">
+          <div
+            className="w-12 h-12 rounded-full mr-3"
+            style={{
+              backgroundSize: "cover",
+              backgroundImage: `url('./${img}.png')`,
+            }}
+          />
+          <div>
+            <p className="font-bold">{name}</p>
+            <p>{info}</p>
+          </div>
+          <div className="absolute right-0 w-[1.125rem] h-1 more-horizontal-icon" />
+        </div>
       ))}
     </div>
   </>
